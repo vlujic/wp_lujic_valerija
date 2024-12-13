60238d3da35f17e1b06b0d55d4859976fe5d4b1b
jazvicana <jazvicana1@gmail.com>
2024-12-13 13:23:59 +0100

    grid i js

---
+++ JavaScript/index.js
@@ -0,0 +1,22 @@
+/* var broj1 =prompt("Unesi prvi broj");
+var broj2 = prompt("Unesi broj 2");
+
+if (broj1> broj2)
+{
+    alert(broj1 +"je veći od broja" + broj2);
+    }
+else if  (broj1 < broj2) {
+    alert(broj2 + " je veći od broja" + broj1);
+}
+else
+{
+  alert("Brojevi su jednaki") ; 
+} */
+
+  //love calculator
+  var ime1 = prompt ("unesi svoje ime:");
+  var ime2 = prompt("unesi ime simpatije:");
+
+  var n= Math.floor(Math.random()*100)+1; //1-100%
+
+  alert("Kompatiblini ste: "+ n + "%" );
